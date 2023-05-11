f8158dd06e55bdaa5c052b1886fdd96c1b37f411
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-05-11 11:35:55 -0300

    ajustes

---
+++ script.js
@@ -0,0 +1,30 @@
+const criarItem = () => {
+    const btnAdicionar = document.querySelector(".js-btn-adicionar");
+    btnAdicionar.addEventListener("click", () => {
+        const input = document.querySelector(".js-input");
+        const inputTarefa = input.value;
+        const item = document.createElement("div");
+        item.classList.add("todo__tarefa");
+        item.innerHTML = `
+        <input type="checkbox" class="todo__checkbox">
+        <p class="todo__tarefa--nome">${inputTarefa}</p>
+        <button class="todo__tarefa--btn">X</button>
+    `
+    const tarefa = document.querySelector(".js-todo-tarefas");
+    tarefa.appendChild(item);
+    });
+}
+
+criarItem()
+
+
+
+
+
+
+
+
+
+{/* <div class="todo__tarefa">
+    
+</div> */}
